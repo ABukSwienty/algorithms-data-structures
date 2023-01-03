9a6cf22016cce00a0f3584d16770524b23a3ddc1
ABukSwienty <alexanderbukswienty@gmail.com>
2023-01-03 13:54:15 +0100

    feat(singly-linked-list)

--- src/singly-linked-list/index.ts
+++ src/singly-linked-list/index.ts
@@ -29,6 +29,42 @@ interface SinglyLinkedListInterface<T> {
    * Removes the first node from the linked list and returns it
    */
   shift(): Node<T> | null;
+
+  /**
+   * Adds a new node to the beginning of the linked list
+   * @param data The data to be added to the linked list
+   */
+  unshift(data: T): SinglyLinkedList<T>;
+
+  /**
+   * Gets the node at the specified index
+   * @param index The index of the node to be returned
+   */
+  get(index: number): Node<T> | null;
+
+  /**
+   * Sets the data of the node at the specified index
+   * @param index The index of the node to be updated
+   * @param data The data to be updated
+   */
+  set(index: number, data: T): boolean;
+
+  /**
+   * Loops through each node in the linked list and executes a callback function with the current node and index
+   * @param callback The callback function to be executed on each node
+   */
+  forEach(callback: (node: Node<T>, index: number) => void): void;
+
+  /**
+   * Removes the node at the specified index
+   * @param index number
+   */
+  remove(index: number): Node<T> | null;
+
+  /**
+   * Reverses the linked list
+   */
+  reverse(): SinglyLinkedList<T>;
 }
 
 class Node<T> {
@@ -72,7 +108,7 @@ class SinglyLinkedList<T> implements SinglyLinkedListInterface<T> {
       return null;
     }
 
-    let temp = this.head.next;
+    let temp = this.head;
     let newTail = this.head;
 
     while (temp?.next) {
@@ -82,7 +118,14 @@ class SinglyLinkedList<T> implements SinglyLinkedListInterface<T> {
 
     newTail.next = null;
     this.tail = newTail;
+
     this.decrementLength();
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
     return temp;
   }
 
@@ -94,6 +137,10 @@ class SinglyLinkedList<T> implements SinglyLinkedListInterface<T> {
     const temp = this.head;
     this.head = this.head.next;
     this.decrementLength();
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
     return temp;
   }
 
@@ -109,6 +156,99 @@ class SinglyLinkedList<T> implements SinglyLinkedListInterface<T> {
     this.head = node;
 
     return this;
+  }
+
+  public get(index: number) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let counter = 0;
+    let current = this.head;
+
+    while (counter !== index) {
+      current = current!.next;
+      counter++;
+    }
+
+    return current;
+  }
+
+  public set(index: number, data: T) {
+    const node = this.get(index);
+
+    if (!node) {
+      return false;
+    }
+
+    node.data = data;
+    return true;
+  }
+
+  public remove(index: number) {
+    if (index < 0 || index >= this.length) return null;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    const prev = this.get(index - 1);
+    const removedNode = prev?.next;
+    const next = this.get(index + 1);
+
+    if (!prev || !removedNode || !next) return null;
+
+    prev.next = next;
+    this.decrementLength();
+
+    return removedNode;
+  }
+
+  public reverse() {
+    if (!this.head) return this;
+
+    // example:
+    // H              T
+    // 1 -> 2 -> 3 -> 4
+    // T              H
+    // 1 <- 2 <- 3 <- 4
+
+    // reverse the head and tail
+    let node: Node<T> | null = this.head;
+    this.head = this.tail;
+    this.tail = node;
+
+    // placeholder values
+    let prev: Node<T> | null = null;
+    let next: Node<T> | null = node.next;
+
+    for (let i = 0; i < this.length; i++) {
+      // sets the next node to the current node's next
+      next = node ? node?.next : null;
+      // sets the current node's next to the previous node
+      if (node) node.next = prev;
+      // updates the previous node to the current node for the loop
+      prev = node;
+      // updates the current node to the next node for the loop
+      node = next;
+    }
+
+    return this;
+  }
+
+  public toArray() {
+    const arr: T[] = [];
+    this.forEach(node => arr.push(node.data));
+
+    return arr;
+  }
+
+  public forEach(callback: (node: Node<T>, index: number) => void) {
+    let current = this.head;
+    let counter = 0;
+
+    while (current) {
+      callback(current, counter);
+      current = current.next;
+      counter++;
+    }
   }
 
   private incrementLength() {
--- test/singly-linked-list.test.ts
+++ test/singly-linked-list.test.ts
@@ -1,8 +1,8 @@
 import SinglyLinkedList from '../src/singly-linked-list';
 
 describe('Test singly linked list', () => {
-  const list = new SinglyLinkedList<number>();
   it('should add a single node and set head and tail', () => {
+    const list = new SinglyLinkedList<number>();
     list.push(1);
     expect(list.getLength()).toBe(1);
     expect(list.getHead()?.data).toBe(1);
@@ -10,28 +10,115 @@ describe('Test singly linked list', () => {
   });
 
   it('should add multiple nodes and update tail', () => {
-    list.push(2);
-    list.push(3);
-    list.push(4);
-    list.push(5);
-    expect(list.getLength()).toBe(5);
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3).push(4);
+    expect(list.getLength()).toBe(4);
   });
 
   it('should remove the last node and update tail', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1);
+
     const popped = list.pop();
-    expect(popped?.data).toBe(5);
+
+    expect(popped?.data).toBe(1);
+
+    expect(list.getLength()).toBe(0);
+    expect(list.getHead()).toBe(null);
+    expect(list.getTail()).toBe(null);
+  });
+
+  it('should try to remove the last item of an empty list and receive null', () => {
+    const list = new SinglyLinkedList<number>();
+
+    const popped = list.pop();
+
+    expect(popped).toBe(null);
+    expect(list.getLength()).toBe(0);
+    expect(list.getHead()).toBe(null);
+    expect(list.getTail()).toBe(null);
   });
 
   it('should remove the first node and update head', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2);
     const shifted = list.shift();
     expect(shifted?.data).toBe(1);
     expect(list.getHead()?.data).toBe(2);
+    list.shift();
+    expect(list.getHead()).toBe(null);
+    expect(list.getTail()).toBe(null);
+    expect(list.getLength()).toBe(0);
+  });
+
+  it('should try and remove the first item of an empty list and receive null', () => {
+    const list = new SinglyLinkedList<number>();
+    const shifted = list.shift();
+    expect(shifted).toBe(null);
+    expect(list.getLength()).toBe(0);
+    expect(list.getHead()).toBe(null);
+    expect(list.getTail()).toBe(null);
   });
 
   it('should add a node at the beginning of the list', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1);
     list.unshift(0);
     expect(list.getHead()?.data).toBe(0);
+  });
 
-    console.log(list);
+  it('should get the node at the specified index', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3);
+    const node = list.get(2);
+    expect(node?.data).toBe(3);
+  });
+
+  it('should set the data of the node at the specified index', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3);
+    const updated = list.set(2, 4);
+    expect(updated).toBe(true);
+    expect(list.get(2)?.data).toBe(4);
+    expect(list.getTail()?.data).toBe(4);
+  });
+
+  it('should loop through the list and fire a callback for each node', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3).push(4);
+    const callback = jest.fn();
+    list.forEach(callback);
+    expect(callback).toHaveBeenCalledTimes(4);
+  });
+
+  it('should remove a node at the specified index', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3).push(4);
+    const removed = list.remove(1);
+    expect(list.getLength()).toBe(3);
+    expect(removed?.data).toBe(2);
+
+    const removed2 = list.remove(1);
+    expect(list.getLength()).toBe(2);
+    expect(removed2?.data).toBe(3);
+  });
+
+  it('should transform list to an array of the data', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3).push(4);
+    const arr = list.toArray();
+
+    expect(arr).toEqual([1, 2, 3, 4]);
+  });
+
+  it('should reverse the list', () => {
+    const list = new SinglyLinkedList<number>();
+    list.push(1).push(2).push(3).push(4);
+
+    expect(list.toArray()).toEqual([1, 2, 3, 4]);
+
+    list.reverse();
+
+    expect(list.toArray()).toEqual([4, 3, 2, 1]);
   });
 });
